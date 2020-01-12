f135b9a54709acfa3e0e1142787397ae49fc46f3
Niusha H <niusha.hadziomerovic@gmail.com>
2020-01-12 23:22:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ class App < Sinatra::Base
     # @analyzed_text = TextAnalyzer.new(text_from_user)
 
     @analyzed_text = TextAnalyzer.new(params[:user_text])
+    "#{@analyzed_text}"
     erb :results
   end
 end
